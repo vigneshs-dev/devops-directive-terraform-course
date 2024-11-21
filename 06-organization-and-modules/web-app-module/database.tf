6b3e077bc3a715dfb9085bfec9dd5820e09468eb
vigneshs-dev <vigneshkavery97@gmail.com>
2024-11-21 09:57:03 +0000

    Update in S3 Bucket name

--- 06-organization-and-modules/web-app-module/database.tf
+++ 06-organization-and-modules/web-app-module/database.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "db_instance" {
   storage_type        = "standard"
   engine              = "postgres"
   engine_version      = "12"
-  instance_class      = "db.t2.micro"
+  instance_class      = "db.t3.micro"
   name                = var.db_name
   username            = var.db_user
   password            = var.db_pass
